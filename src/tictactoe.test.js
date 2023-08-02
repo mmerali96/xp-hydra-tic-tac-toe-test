0ee7067297c269afff0bca61064ab0d03e26a7a2
Mehdi <mmerali@ibm.com>
2023-08-02 14:35:45 -0400

    created test for empty gameboard array

--- src/tictactoe.test.js
+++ src/tictactoe.test.js
@@ -18,4 +18,8 @@ Board Created.`);
   it('should create an object for the tictactoe game', () => {
     var tictactoe = new TicTacToe();
   });
+  it('should have an empty 2d array to represent the gameboard', () => {
+    var tictactoe = new TicTacToe();
+    expect(tictactoe.board).toBe([[], [], []]);
+  });
 });
