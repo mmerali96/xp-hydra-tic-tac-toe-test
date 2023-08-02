72fb5b4017fd53ff3ddf362fcdace28264acd2ae
Mehdi <mmerali@ibm.com>
2023-08-02 14:20:55 -0400

    Created gameboard creation test

--- src/tictactoe.test.js
+++ src/tictactoe.test.js
@@ -4,4 +4,14 @@ describe('tic tac toe tests', () => {
   it('should have a function called tictactoe', () => {
     tictactoe();
   });
+  it('should return an empty gameboard with prefix and postfix strings', () => {
+    expect(tictactoe()).toBe(`
+Game Board Creation...
+ | |
+-+-+-
+ | |
+-+-+-
+ | |
+Board Created.`);
+  });
 });
